bb1c5a58802b6cbd5559772280df88401e774543
Adelin Pachedzhiev <56289229+Adelin-Pachedzhiev@users.noreply.github.com>
2025-06-14 16:31:04 +0300

    fix theme provider

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,11 +1,13 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import React from "react";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import theme from "./theme/CustomTheme";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <GoogleOAuthProvider clientId="565656520042-073n8e4oj9eb5au1q6au0crrgdq6fh07.apps.googleusercontent.com">
         {children}
       </GoogleOAuthProvider>
