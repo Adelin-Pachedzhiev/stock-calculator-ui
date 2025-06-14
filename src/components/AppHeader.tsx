432f3c1609362d5578704dc9754c5dc8329357e6
Adelin Pachedzhiev <56289229+Adelin-Pachedzhiev@users.noreply.github.com>
2025-06-14 16:31:15 +0300

    add header component

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,104 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+  useTheme,
+} from '@mui/material';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+const AppHeader = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const theme = useTheme();
+  const navigate = useNavigate();
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleSettings = () => {
+    handleClose();
+    navigate('/settings');
+  };
+
+  const handleLogout = () => {
+    handleClose();
+    // TODO: Implement logout logic
+  };
+
+  return (
+    <AppBar position="static" elevation={0}>
+      <Toolbar>
+        {/* Logo/Brand */}
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1, cursor: 'pointer' }}
+          onClick={() => navigate('/')}
+        >
+          Stock Portfolio
+        </Typography>
+
+        {/* Navigation Links */}
+        <Box sx={{ display: 'flex', gap: 2, mr: 2 }}>
+          <Button 
+            color="inherit" 
+            onClick={() => navigate('/portfolio')}
+          >
+            Portfolio
+          </Button>
+          <Button 
+            color="inherit" 
+            onClick={() => navigate('/integrations')}
+          >
+            Integrations
+          </Button>
+        </Box>
+
+        {/* Settings Button */}
+        <IconButton
+          size="large"
+          edge="end"
+          color="inherit"
+          onClick={() => navigate('/settings')}
+          sx={{ mr: 1 }}
+        >
+          <SettingsIcon />
+        </IconButton>
+
+        {/* User Menu */}
+        <IconButton
+          size="large"
+          edge="end"
+          color="inherit"
+          onClick={handleMenu}
+        >
+          <AccountCircleIcon />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+          transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        >
+          <MenuItem onClick={handleSettings}>Settings</MenuItem>
+          <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default AppHeader; 
