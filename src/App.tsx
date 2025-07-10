457b7c0f760ffb4df7acc92ac282fef46e312e5f
Adelin Pachedzhiev <56289229+Adelin-Pachedzhiev@users.noreply.github.com>
2025-07-10 14:07:10 +0300

    remove dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
 import Login from "./components/auth/Login";
 import Integrations from "./pages/Integrations";
 import Portfolio from "./pages/Portfolio";
@@ -14,7 +13,7 @@ function App() {
         <AppHeader />
         <Box component="main" sx={{ flexGrow: 1 }}>
           <Routes>
-            <Route path="/" element={<Dashboard />} />
+            <Route path="/" element={<Portfolio />} />
             <Route path="/login" element={<Login />} />
             <Route path="/integrations" element={<Integrations/>}/>
             <Route path="/portfolio" element={<Portfolio/>}/>
